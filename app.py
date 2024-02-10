f0255566f9e9c79c90e4a8797a3b94c0ef6dc3f8
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2024-02-10 16:29:35 -0300

    efetuado carga de dados e apresentado dataframe

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import pygame as pg
+
+def load_data():
+    return pd.read_csv('data/processed/bikes_completed.csv')
+
+def main():
+    df = load_data()
+
+    st.dataframe(df)
+
+if __name__ == '__main__':
+    main()
+    
