5b10d668859a0a917c23f46fffe29666978655f9
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2024-02-11 15:05:35 -0300

    adicionado secao de descricao dos dados

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+def create_dataframe_section(df):
+    st.title("Database Section")
+
+    col_1, col_2 = st.columns(2)
+
+    col_1.header("Database")
+    col_1.dataframe(df, height=530)
+
+    col_2.header("Data Description")
+
+    data_description = """"
+                        | coluna | Descrição |
+                        | :----- | --------: |
+                        | ID | Identificador da linha/registro |
+                        | name | Fabricante e modelo da moto |
+    """
+    col_2.markdown(data_description)
+    return None
+
 def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
